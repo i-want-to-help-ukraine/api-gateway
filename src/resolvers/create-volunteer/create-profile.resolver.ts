11218e8a777ed3cb5c3e1c1ac7dd3a13b15192b8
Alexey Yudin <alexey.y.yudin@gmail.com>
2022-03-12 23:05:33 +0100

    feat: profile

--- src/resolvers/create-volunteer/create-profile.resolver.ts
+++ src/resolvers/create-volunteer/create-profile.resolver.ts
@@ -1,13 +1,13 @@
 import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { IDatasource } from '../../datasources/datasource.interface';
-import { CreateVolunteerInput } from '../../graphql.schema';
 import { VolunteerDto } from '@i-want-to-help-ukraine/protobuf/types/volunteer-service';
+import { CreateProfileInput } from '../../graphql.schema';
 
 @Resolver()
-export class CreateVolunteerResolver {
-  @Mutation('createVolunteer')
-  async createVolunteer(
-    @Args('input') input: CreateVolunteerInput,
+export class CreateProfileResolver {
+  @Mutation('createProfile')
+  async createProfile(
+    @Args('input') input: CreateProfileInput,
     @Context('dataSources') dataSources: IDatasource,
   ): Promise<VolunteerDto | undefined> {
     return dataSources.volunteer.createVolunteer(input);
