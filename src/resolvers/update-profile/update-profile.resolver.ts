75521ca8c345f6972073d42bf4bfe17190fd137b
Alexey Yudin <alexey.y.yudin@gmail.com>
2022-03-17 02:36:29 +0100

    feat: volunteer update

--- src/resolvers/update-profile/update-profile.resolver.ts
+++ src/resolvers/update-profile/update-profile.resolver.ts
@@ -16,6 +16,6 @@ export class UpdateProfileResolver {
     @Context('dataSources') dataSources: IDatasource,
     @CurrentUser() currentUser: Auth0Payload,
   ): Promise<VolunteerDto | undefined> {
-    return dataSources.volunteer.updateProfile(input.volunteerId, input);
+    return dataSources.volunteer.updateProfile(currentUser.authId, input);
   }
 }
